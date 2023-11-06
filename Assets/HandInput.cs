d762c9c6f8a9e51ba2df632b4b8b905083c141f3
Averageasd <nhuynh9497@gmail.com>
2023-11-06 16:08:31 -0500

    made the sphere smaller

---
+++ Assets/HandInput.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandInput : MonoBehaviour
+{
+    public InputActionProperty inputActions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
