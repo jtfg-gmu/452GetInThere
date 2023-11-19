27d6d751b0fbf1c8619002799e9b55674c4baea7
cpapage <cpapage@gmu.edu>
2023-11-19 13:52:40 -0500

    Update FollowComponent.cs

--- Assets/Scripts/FollowComponent.cs
+++ Assets/Scripts/FollowComponent.cs
@@ -5,24 +5,33 @@ using UnityEngine.AI;
 
 public class FollowComponent : MonoBehaviour
 {
-    private NavMeshAgent agent;
-    private GameObject dest;
-    
+    public NavMeshAgent navMesh;
+    public int Follow_Distance;
+    private bool is_moving; 
+    private Animator my_animator;
+
+    public bool isMoving()
+    {
+        return navMesh.remainingDistance <= navMesh.stoppingDistance;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-	    dest = GameObject.FindGameObjectWithTag("castle");
-	    agent = GetComponent<NavMeshAgent>();
-	    agent.SetDestination(dest.transform.position);
+        navMesh = this.GetComponent<NavMeshAgent>();
+    	my_animator = this.GetComponent<Animator>();
+    	is_moving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
- //        double distance = Vector3.Distance(this.gameObject.transform.position, Main.instance.castle.position);
-	// if(distance <= Follow_Distance)
-	//     navMesh.SetDestination(Main.instance.transform.position);
-	// is_moving = isMoving();
+        double distance = Vector3.Distance(this.gameObject.transform.position, Main.instance.castle.position);
+    	if(distance <= Follow_Distance)
+    	    navMesh.SetDestination(Main.instance.castle.transform.position);
+    	else
+    	    //go to the player.
+    	is_moving = isMoving();
+    	my_animator.SetBool("is_moving", is_moving);
     }
 }
