19a3e4fcd4e98eb1121f5e5a4c7f1751592be429
January N <inelsona@gmail.com>
2023-11-19 18:53:20 -0500

    Fix errors caused by push (minor refactor)

--- Assets/Scripts/FollowComponent.cs
+++ Assets/Scripts/FollowComponent.cs
@@ -5,9 +5,16 @@ using UnityEngine.AI;
 
 public class FollowComponent : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public NavMeshAgent navMesh;
     public int Follow_Distance;
     private bool is_moving; 
+=======
+    Main m;
+    public NavMeshAgent navMesh;
+    public int Follow_Distance;
+    private bool is_moving;
+>>>>>>> Stashed changes
     private Animator my_animator;
 
     public bool isMoving()
@@ -18,15 +25,23 @@ public class FollowComponent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         navMesh = this.GetComponent<NavMeshAgent>();
     	my_animator = this.GetComponent<Animator>();
     	is_moving = false;
+=======
+        m = Main.instance;
+        navMesh = this.GetComponent<NavMeshAgent>();
+        my_animator = this.GetComponent<Animator>();
+        is_moving = false;
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
         double shortestDistance = 2000000;
+<<<<<<< Updated upstream
 	Transform closestCastle = null;
 
 	for(var i = 0; i < Main.instance.castles.Length; i++)
@@ -43,5 +58,23 @@ public class FollowComponent : MonoBehaviour
     	    navMesh.SetDestination(closestCastle.position);
     	is_moving = isMoving();
     	my_animator.SetBool("is_moving", is_moving);
+=======
+        Transform closestCastle = null;
+
+        for (var i = 0; i < m.castleLocations.Length; i++)
+        {
+            double distance = Vector3.Distance(this.gameObject.transform.position, m.castleLocations[i].transform.position);
+            if (distance <= shortestDistance)
+            {
+                shortestDistance = distance;
+                closestCastle = m.castleLocations[i];
+            }
+        }
+
+        if (shortestDistance <= Follow_Distance)
+            navMesh.SetDestination(closestCastle.position);
+        is_moving = isMoving();
+        my_animator.SetBool("is_moving", is_moving);
+>>>>>>> Stashed changes
     }
 }
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
 	public static Main instance;
+<<<<<<< Updated upstream
 	public Transform[] castles;
 	public Transform Soldier_Spawn;
 	public int Number_of_allied_soldiers;
 	private int soldierCtr;
+=======
+	[SerializeField] public Transform[] castleLocations;
+    [SerializeField] Transform soldierSpawnLocation;
+	
+	[SerializeField] int soldierCountMax;
+	private int soldierCount;
+
+	//Something similar can be implemented for enemy soldiers.
+
+    private float nextWaveDefault = 3.0f;
+    private float nextWaveTimer;
+>>>>>>> Stashed changes
 
 	void Awake()
 	{
@@ -26,7 +39,30 @@ public class Main : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+<<<<<<< Updated upstream
 		
+=======
+		if (!IsInvoking("CreateSoldier"))
+		{
+			//if failed spawn wave, make checking timer longer until a change in gamestate occurs
+			nextWaveTimer += nextWaveDefault;
+			SpawnWave(nextWaveTimer);
+		}
+	}
+	
+
+	//Spawns castles (at the start of the game).
+	private void CreateCastles()
+	{
+		GameObject go;
+		if(castleLocations.Length == 0) { Debug.LogError("No castle locations initialized!"); return;  }
+		for (var i = 0; i < castleLocations.Length; i++)
+		{
+			go = Instantiate(Resources.Load<GameObject>("Castle"), castleLocations[i].position, castleLocations[i].rotation);
+			go.name = "Castle " + i;
+			//other modifications here.
+		}
+>>>>>>> Stashed changes
 	}
 
 	private void SpawnSoilders()
