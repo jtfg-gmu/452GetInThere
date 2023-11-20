33fee0177b2b79e3ae806f50c25ba4be0f4e6195
January N <inelsona@gmail.com>
2023-11-19 19:10:14 -0500

    Fix errors caused by push (for real this time)

--- Assets/Scripts/FollowComponent.cs
+++ Assets/Scripts/FollowComponent.cs
@@ -5,16 +5,11 @@ using UnityEngine.AI;
 
 public class FollowComponent : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    public NavMeshAgent navMesh;
-    public int Follow_Distance;
-    private bool is_moving; 
-=======
+
     Main m;
     public NavMeshAgent navMesh;
     public int Follow_Distance;
     private bool is_moving;
->>>>>>> Stashed changes
     private Animator my_animator;
 
     public bool isMoving()
@@ -25,40 +20,16 @@ public class FollowComponent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< Updated upstream
-        navMesh = this.GetComponent<NavMeshAgent>();
-    	my_animator = this.GetComponent<Animator>();
-    	is_moving = false;
-=======
         m = Main.instance;
         navMesh = this.GetComponent<NavMeshAgent>();
         my_animator = this.GetComponent<Animator>();
         is_moving = false;
->>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
         double shortestDistance = 2000000;
-<<<<<<< Updated upstream
-	Transform closestCastle = null;
-
-	for(var i = 0; i < Main.instance.castles.Length; i++)
-	{
-		double distance = Vector3.Distance(this.gameObject.transform.position, Main.instance.castles[i].transform.position);
-	    	if(distance <= shortestDistance)
-		{
-		    shortestDistance = distance;
-		    closestCastle = Main.instance.castles[i];
-		}
-	}
-
-	if(shortestDistance <= Follow_Distance)
-    	    navMesh.SetDestination(closestCastle.position);
-    	is_moving = isMoving();
-    	my_animator.SetBool("is_moving", is_moving);
-=======
         Transform closestCastle = null;
 
         for (var i = 0; i < m.castleLocations.Length; i++)
@@ -75,6 +46,5 @@ public class FollowComponent : MonoBehaviour
             navMesh.SetDestination(closestCastle.position);
         is_moving = isMoving();
         my_animator.SetBool("is_moving", is_moving);
->>>>>>> Stashed changes
     }
 }
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,87 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-	public static Main instance;
-<<<<<<< Updated upstream
-	public Transform[] castles;
-	public Transform Soldier_Spawn;
-	public int Number_of_allied_soldiers;
-	private int soldierCtr;
-=======
-	[SerializeField] public Transform[] castleLocations;
-    [SerializeField] Transform soldierSpawnLocation;
-	
-	[SerializeField] int soldierCountMax;
-	private int soldierCount;
 
-	//Something similar can be implemented for enemy soldiers.
+
+    public static Main instance;
+    [SerializeField] public Transform[] castleLocations;
+    [SerializeField] Transform soldierSpawnLocation;
+
+    [SerializeField] int soldierCountMax;
+    private int soldierCount;
+
+    //Something similar can be implemented for enemy soldiers.
 
     private float nextWaveDefault = 3.0f;
     private float nextWaveTimer;
->>>>>>> Stashed changes
 
-	void Awake()
-	{
-		Main.instance = this;
-	}
+    void Awake()
+    {
+        instance = this;
+    }
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		soldierCtr = 0;
-		SpawnCastles();
-		InvokeRepeating("SpawnSoilders", 1f, 3f);
-	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextWaveTimer = nextWaveDefault;
+        soldierCount = 0;
+        CreateCastles();
+        SpawnWave();
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-<<<<<<< Updated upstream
-		
-=======
-		if (!IsInvoking("CreateSoldier"))
-		{
-			//if failed spawn wave, make checking timer longer until a change in gamestate occurs
-			nextWaveTimer += nextWaveDefault;
-			SpawnWave(nextWaveTimer);
-		}
-	}
-	
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsInvoking("CreateSoldier"))
+        {
+            //if failed spawn wave, make checking timer longer until a change in gamestate occurs
+            nextWaveTimer += nextWaveDefault;
+            SpawnWave(nextWaveTimer);
+        }
+    }
 
-	//Spawns castles (at the start of the game).
-	private void CreateCastles()
-	{
-		GameObject go;
-		if(castleLocations.Length == 0) { Debug.LogError("No castle locations initialized!"); return;  }
-		for (var i = 0; i < castleLocations.Length; i++)
-		{
-			go = Instantiate(Resources.Load<GameObject>("Castle"), castleLocations[i].position, castleLocations[i].rotation);
-			go.name = "Castle " + i;
-			//other modifications here.
-		}
->>>>>>> Stashed changes
-	}
 
-	private void SpawnSoilders()
-	{
-		if (soldierCtr <= Number_of_allied_soldiers)
-		{
-			GameObject.Instantiate(Resources.Load<GameObject>("Soldier"), Soldier_Spawn.position, Soldier_Spawn.rotation);
-			soldierCtr += 1;
-		}
-		
-	}
+    //Spawns castles (at the start of the game).
+    private void CreateCastles()
+    {
+        GameObject go;
+        if (castleLocations.Length == 0) { Debug.LogError("No castle locations initialized!"); return; }
+        for (var i = 0; i < castleLocations.Length; i++)
+        {
+            go = Instantiate(Resources.Load<GameObject>("Castle"), castleLocations[i].position, castleLocations[i].rotation);
+            go.name = "Castle " + i;
+            //other modifications here.
+        }
+    }
 
-	private void SpawnCastles()
-	{
-		for (var i = 0; i < castles.Length; i++)
-		{
-			GameObject.Instantiate(Resources.Load<GameObject>("Castle"), castles[i].position, castles[i].rotation);
-		}
-		
-		
-	}
+    //Begins spawning singleton soldiers after a delay.
+    //Spawns up to soldierCountMax and then stops.
+    private void SpawnWave(float delay = 1.0f) { InvokeRepeating("CreateSoldier", delay, 0.5f); }
+
+    //Spawns a soldier.
+    private void CreateSoldier()
+    {
+        if (soldierCount <= soldierCountMax) //if we can still spawn another soldier
+        {
+            GameObject.Instantiate(Resources.Load<GameObject>("Soldier"), soldierSpawnLocation.position, soldierSpawnLocation.rotation);
+            soldierCount += 1;
+        }
+        else
+        {
+            Debug.Log("Skipped soldier spawn due to max number of soldiers");
+            this.CancelInvoke();
+        }
+
+    }
+
+    /*
+ * Extendable Calls
+ */
+
+    //Used by other classes to signal when a soldier despawns (so we can spawn another).
+    public void SignalDespawnSoldier() { soldierCount--; }
+    //Used by other classes to signal when spawntimer should reset to default (i.e. after a lot of time)
+    public void SignalResetSpawns() { nextWaveTimer = nextWaveDefault; }
 }
